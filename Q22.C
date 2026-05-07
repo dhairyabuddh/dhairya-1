282a117064258f5102a66338696bc9f5f78ed12a
anjalidhapa <cosmos.bvn@gmail.com>
2026-05-07 19:51:27 +0530

    changed void

--- Q22.C
+++ Q22.C
@@ -1,49 +1,47 @@
-#include<stdio.h>
+#include <stdio.h>
 
-void main() {
-	int sub1,sub2,sub3,sub4,sub5;
+int main()
+{
+	int sub1, sub2, sub3, sub4, sub5;
 	int total;
 	float perc;
-	
+
 	printf("Enter sub1 marks:");
-	scanf("%d",&sub1);
+	scanf("%d", &sub1);
 
 	printf("Enter sub2 marks:");
-	scanf("%d",&sub2);
+	scanf("%d", &sub2);
 
 	printf("Enter sub3 marks:");
-	scanf("%d",&sub3);
+	scanf("%d", &sub3);
 
 	printf("Enter sub4 marks:");
-	scanf("%d",&sub4);
+	scanf("%d", &sub4);
 
 	printf("Enter sub5 marks:");
-	scanf("%d",&sub5);
+	scanf("%d", &sub5);
 
-	total=sub1+sub2+sub3+sub4+sub5;
-	printf("total=%d \n",total);
+	total = sub1 + sub2 + sub3 + sub4 + sub5;
+	printf("total=%d \n", total);
 
-	perc=(float)total/5;
-	printf("perc=%f \n",perc);
-	if(sub1>=40 && sub2>=40 && sub3>=40 && sub4>=40 && sub5>=40)
+	perc = (float)total / 5;
+	printf("perc=%f \n", perc);
+	if (sub1 >= 40 && sub2 >= 40 && sub3 >= 40 && sub4 >= 40 && sub5 >= 40)
 	{
-		if(perc>=40 && perc<45)
+		if (perc >= 40 && perc < 45)
 			printf("You are in pass class");
-		else if(perc>=45 && perc<60)
+		else if (perc >= 45 && perc < 60)
 			printf("You are in second class");
-		else if(perc>=60 && perc<75)
+		else if (perc >= 60 && perc < 75)
 			printf("You are in first class");
-		else if(perc>=75 && perc<90)
+		else if (perc >= 75 && perc < 90)
 			printf("You are in distinction");
-		else if(perc>=90)
+		else if (perc >= 90)
 			printf("You are in merit");
 	}
 	else
 	{
 		printf("\nfail");
 	}
-
-
-
-
+	return 0;
 }
