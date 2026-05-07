c0fffbcaedd7dfb589dd3d5f19654157c796e7bd
anjalidhapa <cosmos.bvn@gmail.com>
2026-05-07 19:39:08 +0530

    updated loop1

--- LOOP1.C
+++ LOOP1.C
@@ -1,25 +1,22 @@
 #include <stdio.h>
-#include <conio.h>
 
-void main() {
+int main() {
 	int num = 1;
-	clrscr();
-
-	while (num <= 10) {
+	
+	while (10 == 10) {
 		printf("%d ", num);
 		num++; // num += 1; // num = num + 1;
-
 	}
-
+	
 	/*
 	++ -> num += 1 -> num = num + 1
 	-- -> num -= 1 -> num = num - 1
-
+	
 	+=
 	-=
 	*=
 	/=
 	%=
 	*/
-	getch();
+	return 0;
 }
