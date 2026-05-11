af225a5259ea80bfc304b12e4059271f044ebbb7
anjalidhapa <cosmos.bvn@gmail.com>
2026-05-11 19:15:34 +0530

    string length

--- Q64.c
+++ Q64.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 int getlength (char name[],int size) {
-    int i,l;
+    int i,l=0;
     for (i=0;name[i] != '\0';i++) {
         l ++;
     }
@@ -8,9 +8,8 @@ int getlength (char name[],int size) {
 }
 int main () {
     char name[20];
-    int l;
+    int l=0;
     printf("enter number :");
     scanf("%s",&name);
-    printf("lenght of string = %d",l);
-    printf("%d",getlength (name,29));
+    printf("lenght of string = %d",getlength (name,29));
 }
