cc60137e2e51261337ddf55fdf276cee46af7218
Dhairya Buddh <dhairyabuddh09@gmail.com>
2026-05-15 14:16:13 +0530

    copy

--- practiceinvs.c
+++ practiceinvs.c
@@ -1,5 +1,13 @@
 #include<stdio.h>
 int main () {
-    int  
+    int  i,nu1,nu2;
+    for (i = 1;i <= 30 ; i = i+2 ) {
+        printf("%d",i);
+    }
+    printf("below number is odd .");
+    for (i = 2; i <= 30; i = i + 2 ) {
+        printf("%d",i);
+    }
+    return 0;
 }
 
